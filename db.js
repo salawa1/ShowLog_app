05e330d671da51fdbae3ac1a76e0749ce9e5297c
salawa1 <156034252+salawa1@users.noreply.github.com>
2025-02-04 20:09:41 -0500

    Update db.js
    Another formatting mistake

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "Postgres",
     password: "ALnUIZa6mFfcMYSilL0dMTNrcB4wHBCN",
-    host: "@dpg-cucmf39opnds73aj7eq0-a",
+    host: "dpg-cucmf39opnds73aj7eq0-a",
     database: "ShowLog",
     port: 5432,
 });
