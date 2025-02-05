560600229a0e301afd9de6bbd7e154fc1402bf0a
salawa1 <156034252+salawa1@users.noreply.github.com>
2025-02-04 20:08:11 -0500

    Update db.js
    This points to the render environment

--- db.js
+++ db.js
@@ -2,8 +2,9 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "Postgres",
-    password: "Amat1966",
-    host: "localhost",
+    password: "ALnUIZa6mFfcMYSilL0dMTNrcB4wHBCN",
+    host: "@dpg-cucmf39opnds73aj7eq0-a
+",
     database: "ShowLog",
     port: 5432,
 });
